261ef98b34f138034a1f79da3c95c4425fe51368
mehul kothari <mehul.kothari@growth99.com>
2025-04-20 15:49:51 +0530

    changes fro adding token reveerted

--- src/app/components/cke-editor5/cke-editor5.component.ts
+++ src/app/components/cke-editor5/cke-editor5.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ViewChild, ViewEncapsulation } from '@angular/core';
 import {
 	type EditorConfig,
 	ClassicEditor,
@@ -47,6 +47,9 @@ import {
 	TextTransformation
 } from 'ckeditor5';
 
+const LICENSE_KEY =
+	'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NDYzMTY3OTksImp0aSI6Ijc2NjBlMmQ3LWMxN2MtNGFiMi05YjkxLWNmN2ZjMmViN2M2OSIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6IjA1MDU2M2UyIn0.mI9eFtgxmPk7JqmRi038f9MRdt2loOWeRDfimn1id3I4cG8oIcDIBAxo5lA1UCbRM28jS-VoL6osjpL3qjL3tg';
+
 const CLOUD_SERVICES_TOKEN_URL =
 	'https://kz86ruuztk7s.cke-cs.com/token/dev/712cfe6299b235e20caa1bc934c9df557852ba73c9ab89c769d98cf52e73?limit=10';
 
@@ -59,13 +62,8 @@ const CLOUD_SERVICES_TOKEN_URL =
   encapsulation: ViewEncapsulation.None
 
 })
-export class CkeEditor5Component implements OnInit, AfterViewInit {
+export class CkeEditor5Component implements AfterViewInit {
 	constructor(private changeDetector: ChangeDetectorRef) {}
-	ngOnInit(): void {
-		console.log('here', process.env);
-		console.log('here', process.env['CKEDITOR_LICENSE_KEY']);
-
-	}
   @ViewChild('ckeditor') ckeditorComponent: any;
 
 	public isLayoutReady = false;
@@ -216,7 +214,7 @@ export class CkeEditor5Component implements OnInit, AfterViewInit {
 			},
 			initialData:
 				'<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n\tYou\'ve successfully created a CKEditor 5 project. This powerful text editor\n\twill enhance your application, enabling rich text editing capabilities that\n\tare customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n\t<li>\n\t\t<strong>Integrate into your app</strong>: time to bring the editing into\n\t\tyour application. Take the code you created and add to your application.\n\t</li>\n\t<li>\n\t\t<strong>Explore features:</strong> Experiment with different plugins and\n\t\ttoolbar options to discover what works best for your needs.\n\t</li>\n\t<li>\n\t\t<strong>Customize your editor:</strong> Tailor the editor\'s\n\t\tconfiguration to match your application\'s style and requirements. Or\n\t\teven write your plugin!\n\t</li>\n</ol>\n<p>\n\tKeep experimenting, and don\'t hesitate to push the boundaries of what you\n\tcan achieve with CKEditor 5. Your feedback is invaluable to us as we strive\n\tto improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n\t<li>📝 <a href="https://portal.ckeditor.com/checkout?plan=free">Trial sign up</a>,</li>\n\t<li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n\t<li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n\t<li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n\t<li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n\tSee this text, but the editor is not starting up? Check the browser\'s\n\tconsole for clues and guidance. It may be related to an incorrect license\n\tkey if you use premium features or another feature-related requirement. If\n\tyou cannot make it work, file a GitHub issue, and we will help as soon as\n\tpossible!\n</p>\n',
-			licenseKey: process.env['CKEDITOR_LICENSE_KEY'] || '',
+			licenseKey: LICENSE_KEY,
 			link: {
 				addTargetToExternalLinks: true,
 				defaultProtocol: 'https://',
